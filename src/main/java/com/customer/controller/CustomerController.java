fed05727ce4344007dcad8ff8cf90a99692e9f15
HemanthAndru <48893117+HemanthAndru@users.noreply.github.com>
2021-09-22 16:02:32 +0530

    Update CustomerController.java

--- src/main/java/com/customer/controller/CustomerController.java
+++ src/main/java/com/customer/controller/CustomerController.java
@@ -1,7 +1,7 @@
 package com.customer.controller;
 
 import java.util.List;
-
+#adding comment
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
